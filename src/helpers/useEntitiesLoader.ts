a0439d79dee55bc0e70874f9c2ee505ac7a4a633
Egor Grushin <egorgrushin@gmail.com>
2023-02-20 18:59:22 +0300

    create affectState pipe, create entities loading flow - useEntitiesLoader now works in different way, add refresh button, minor changes,

--- src/helpers/useEntitiesLoader.ts
+++ src/helpers/useEntitiesLoader.ts
@@ -1,43 +1,75 @@
-import { useState } from 'react';
-import { ILoadingState } from '../core.types';
+import { useEffect, useMemo, useRef, useState } from 'react';
+import { IDictionary, ILoadingState } from '../core.types';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { MD5 } from 'object-hash';
+import { map, switchMap } from 'rxjs/operators';
+import { affectState, IAffectStateSetterArg } from './affectState';
 
-export const useEntitiesLoader = <T>(fetcher: (...args: any[]) => Promise<T[]>): [
-	T[],
+const getKeyFromObject = <T>(value?: T): string => MD5(value);
+
+interface IEntitiesLoaderPagePatch<T> {
+	entities?: T[];
+	next: boolean;
+}
+
+export const useEntitiesLoader = <T, F = any>(
+	filterObj: F | undefined,
+	dataSource$Factory: (value: F | undefined, from: number, to: number) => Observable<T[]>,
+	pageSize: number = 50,
+): [
+	T[] | undefined,
 	ILoadingState,
-	(...args: any[]) => Promise<void>,
-	(...args: any[]) => Promise<void>,
+	BehaviorSubject<boolean>,
 ] => {
-	const [entities, setEntities] = useState();
 	const [loadingState, setLoadingState] = useState<ILoadingState>({});
+	// (yrgrushi): paginator instance must depends filterObj in order to reset last emitted 'next' value
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	const paginator = useMemo(() => new BehaviorSubject<boolean>(false), [filterObj]);
+	const [pages, setPages] = useState<IDictionary<T[]>>({});
 
-	const invokeFetcher = async (...args: any[]): Promise<T[]> => {
-		setLoadingState({ isLoading: true });
-		try {
-			const res = await fetcher(...args);
-			setLoadingState({ isLoading: false });
-			return res;
-		} catch (err) {
-			setLoadingState({ isLoading: false, error: err.message });
-		}
-		return [];
-	};
+	const key = getKeyFromObject<F>(filterObj);
+	const page = pages[key];
 
-	const load = async (...args: any[]): Promise<void> => {
-		setEntities([]);
-		const res = await invokeFetcher(...args);
-		setEntities(res);
-	};
+	const pageRef = useRef(page);
+	pageRef.current = page;
 
-	const loadMore = async (...args: any[]): Promise<void> => {
-		const from = entities.length;
-		const res = await invokeFetcher(...args, from);
-		setEntities(entities.concat(res));
-	};
+	const loadingStateSetter = ({ isLoading, error }: IAffectStateSetterArg<Error>): void => setLoadingState({
+		isLoading,
+		error: error?.message,
+	});
+
+	const setter$ = useMemo(
+		(): Observable<IEntitiesLoaderPagePatch<T>> => paginator.pipe(
+			switchMap((next: boolean) => {
+				const from = next ? pageRef.current?.length ?? 0 : 0;
+				const to = from + pageSize;
+				return dataSource$Factory(filterObj, from, to).pipe(
+					affectState<T[]>(loadingStateSetter),
+					map((entities: T[]) => ({ entities, next })),
+				);
+			}),
+		),
+		[pageRef, paginator, pageSize, filterObj, dataSource$Factory],
+	);
+
+	useEffect(() => {
+		const sub = setter$.subscribe((pagePatch) => {
+			setPages((prevPages) => {
+				// FIXME(yrgrushi): refactor this shit
+				let newEntities = pagePatch.entities || [];
+				if (pagePatch.next === false) return { ...prevPages, [key]: newEntities };
+				newEntities = (prevPages[key] || []).concat(newEntities);
+				return { ...prevPages, [key]: newEntities };
+			});
+		});
+		return (): void => sub.unsubscribe();
+		// (yrgrushi:) setter$ depends on key, so it is not necessary to recreate this effect on key change
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [setPages, setter$]);
 
 	return [
-		entities,
+		page,
 		loadingState,
-		load,
-		loadMore,
+		paginator,
 	];
 };
--- src/shared/list/List.types.ts
+++ src/shared/list/List.types.ts
@@ -2,13 +2,14 @@ import { Id, ILoadingState } from '../../core.types';
 import { ReactNode } from 'react';
 
 export interface IListProps<T = any> {
-	items: T[];
+	items: T[] | undefined;
 	options?: Partial<IListOptions>;
 	loadingState?: ILoadingState;
 	selected?: Id[];
 	onSelect?: (item: T) => void;
 	onSelectedChange?: (selected: Id[], item: T, value: boolean) => void;
 	itemTemplate: (item: T) => ReactNode;
+	className?: string;
 }
 
 export interface IListOptions {
